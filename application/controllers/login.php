2be0089239c67e2126b6035e542b382c8e9076b2
veryweh <veryandika1234@gmail.com>
2024-07-10 15:23:50 +0800

    tambah file Login dan lain-lain

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -6,5 +6,25 @@ class Login extends CI_Controller {
         $this->load->view('v_login');
     }
 
-    
+    public function login_aksi(){
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+
+        $where = array(
+            'username'  => $username,
+            'password'  => $password
+        );
+
+        $this->load->model('M_data');
+        $cek = $this->M_data->cek_login('petugas', $where)->num_rows();
+        if($cek > 0){
+            redirect(base_url().'dashboard');
+        }else{
+            redirect(base_url().'login?alert=gagal');
+        }
+    }
+
+    public function register(){
+        $this->load->view('v_register');
+    }
 }
---
+++ application/views/v_login.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+
+<!-- =========================================================
+* Sneat - Bootstrap 5 HTML Admin Template - Pro | v1.0.0
+==============================================================
+
+* Product Page: https://themeselection.com/products/sneat-bootstrap-html-admin-template/
+* Created by: ThemeSelection
+* License: You must have a valid license purchased in order to legally use the theme for your project.
+* Copyright ThemeSelection (https://themeselection.com)
+
+=========================================================
+ -->
+<!-- beautify ignore:start -->
+<html
+  lang="en"
+  class="light-style customizer-hide"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="../assets/"
+  data-template="vertical-menu-template-free"
+>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
+
+    <title>Login Basic - Pages | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="<?php base_url()?>assets_2/assets/img/favicon/favicon.ico" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="<?php base_url(); ?>assets_2/assets/vendor/fonts/boxicons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="<?php base_url(); ?>assets_2/assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="<?php base_url(); ?>assets_2/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="<?php base_url(); ?>assets_2/assets/css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="<?php base_url(); ?>assets_2/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="<?php base_url(); ?>assets_2/assets/vendor/css/pages/page-auth.css" />
+    <!-- Helpers -->
+    <script src="<?php base_url(); ?>assets_2/assets/vendor/js/helpers.js"></script>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="<?php base_url(); ?>assets_2/assets/js/config.js"></script>
+  </head>
+
+  <body>
+    <!-- Content -->
+
+    <div class="container-xxl">
+      <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+          <!-- Register -->
+          <div class="card">
+            <div class="card-body">
+
+              <h4 class="mb-6">Welcome to Sistem Pendukung Keputusan! 👋</h4>
+
+              <?php
+                if(isset($_GET['alert'])){
+                  if($_GET['alert']=="gagal"){
+                    echo "<div class='alert alert-danger alert-dismissible' role='alert'>
+                            Maaf! Username & Password Salah
+                            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                          </div>";
+                  }else if($_GET['alert']=="belum_login"){
+                    echo "<div class='alert alert-danger alert-dismissible' role='alert'>
+                            Anda Harus Login Terlebih Dahulu!
+                            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                          </div>";
+                  }else if($_GET['alert']=="logout"){
+                    echo "<div class='alert alert-success alert-dismissible' role='alert'>
+                            Anda Telah Logout!
+                            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                          </div>";
+                  }
+                }
+              ?>
+
+              <form id="formAuthentication" class="mb-3" action="<?php echo base_url().'login/login_aksi' ?>" method="post">
+                <div class="mb-3">
+                  <label for="username" class="form-label">Username</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="username"
+                    name="username"
+                    placeholder="Enter your username"
+                    required
+                    autofocus
+                  />
+                </div>
+                <div class="mb-3 form-password-toggle">
+                  <div class="d-flex justify-content-between">
+                    <label class="form-label" for="password">Password</label>
+                    <a href="auth-forgot-password-basic.html">
+                      <small>Forgot Password?</small>
+                    </a>
+                  </div>
+                  <div class="input-group input-group-merge">
+                    <input
+                      type="password"
+                      id="password"
+                      class="form-control"
+                      name="password"
+                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                      required
+                      aria-describedby="password"
+                    />
+                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                  </div>
+                </div>
+                <div class="mb-3">
+                  <input type="submit" class="btn btn-primary d-grid w-100" type="submit" value="Login">
+                </div>
+              </form>
+
+              <p class="text-center">
+                <span>New on our platform?</span>
+                <a href="<?php echo base_url().'login/register'; ?>">
+                  <span>Create an account</span>
+                </a>
+              </p>
+            </div>
+          </div>
+          <!-- /Register -->
+        </div>
+      </div>
+    </div>
+
+    <!-- / Content -->
+
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="<?php base_url(); ?>assets_2/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="<?php base_url(); ?>assets_2/assets/vendor/libs/popper/popper.js"></script>
+    <script src="<?php base_url(); ?>assets_2/assets/vendor/js/bootstrap.js"></script>
+    <script src="<?php base_url(); ?>assets_2/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="<?php base_url(); ?>assets_2/assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+    <!-- Main JS -->
+    <script src="<?php base_url(); ?>assets_2/assets/js/main.js"></script>
+
+    <!-- Page JS -->
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+  </body>
+</html>
